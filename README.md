bf8d3f257f7b27c395aa32f9506bc199d4d618b6
Jeremy Mouton <jeremy@aspendigital.com>
2013-04-26 17:17:04 -0600

    Refactored js and polished README

--- README.md
+++ README.md
@@ -2,22 +2,26 @@
 
 *Simple JSON feed parser for Tumblr blogs.*
 
-Working example: [www.jeremymouton.com](http://www.jeremymouton.com/)
-
 - Styled with [Twitter Bootstrap](twitter.github.com/bootstrap/).
 - Using [Moment.js](http://momentjs.com) to format the date.
 - Using [FontAwesome](http://fortawesome.github.io/Font-Awesome/) for post type icons.
 
 ### HOW TO:
 
-To pull in another feed edit ````var blog_url ```` with ````http://YOURBLOG.tumblr.com````.
+1. Edit ``var blog_name `` with ``YOURBLOGNAME`` in ``tumblr-parser.js``. If your blog address is 'http://nyancat.tumblr.com/', your blog name is "nyancat".
 
-Use this script on your own page and feed it using recipes from [IFTTT](https://ifttt.com/):
+To turn it into the ultimate social aggregator feed, create some recipes with [IFTTT](https://ifttt.com/) and automatically post anything you want to your Tumblr:
 
 - Dropbox to Tumblr [recipe](https://ifttt.com/recipes/90662)
 - Evernote to Tumblr [recipe](https://ifttt.com/recipes/90663)
-- Create an RSS recipe to publish your Twitter and Github feeds to Tumblr.
-- To publish Spotify tracks, checkout the Sharing options within the Spotify app, you can post audio tracks to Tumblr (much better than Last.fm integration).
+- Create RSS recipes to publish your RSS feed (Twitter and Github?).
+- For Spotify tracks, checkout the "sharing" options within the Spotify app [support link](http://support.spotify.com/us/learn-more/guides/#!/article/Sharing-music).
+- For the markdown junkies: [Markable](http://markable.in/).
+
+### WHAT?
+
+It's just a feed. You can see a working example on [www.jeremymouton.com](http://www.jeremymouton.com/).
 
 ### TODO:
 - Code syntax hightlighting for Github Gists.
+- Add "Chat" post option.
--- tumblr-parser.js
+++ tumblr-parser.js
@@ -1,79 +1,85 @@
-// Post parser for Tumblr blogs.
-// Change "blog_url" to pull in  a new feed. 
+$( function() {
+	getPosts();
+});	
 
-var blog_url    = 'http://jeremymouton.tumblr.com';
-var tumblr_feed = blog_url + '/api/read/json?callback=?';
+function getPosts() {
+	// Post parser for Tumblr blogs.
+	// Change "blog_url" to pull in  a new feed. 
 
-$.getJSON(tumblr_feed, function(data) {
-	getPosts(data.posts);
-});
+	var blog_name   = 'demo',
+	    tumblr_feed = 'http://' + blog_name + '.tumblr.com/api/read/json?callback=?';
 
-// HELPERS
-function getPosts(json) {
-	for( var i=0, l=json.length; i<l; i++ ) {
-		formatPost(json[i]);
-	}
-}
-
-function formatPost(post) {
-	date    = moment.unix(post['unix-timestamp']).format('MMMM Do YYYY');
-	content = post['regular-body'];
-	type    = post.type;
-
-	// Format post based on post type
-	switch (type)
-	{
-	// Normal post
-	case "regular":
-		content = '<h3>' + post['regular-title'] + '</h3>' + post['regular-body'];
-		icon    = 'icon-align-left';
-		break;
-
-	// Link post
-	case "link":
-		content = '<span class="link-text"><a href="' + post['link-url'] + 
-		          '" target="_blank">' + post['link-text'] + '</a></span>' +
-		          '<span class="link-description"> ' + post['link-description'] + '</span>';
-		icon    = 'icon-link';
-		break;
-
-	// Video post
-	case "video":
-		content = '<div class="video-container">' + post['video-player-500'] + '</div><span class="video-caption">' + post['video-caption'] + '</span>';
-		icon    = 'icon-film';
-		break;
-
-	// Photo post
-	case "photo":
-		content = '<div class="photo"><img src="' + post['photo-url-1280'] + 
-		          '" alt="' + post['photo-caption'] + '"></div>' +
-		          '<span class="photo-caption">' + post['photo-caption'] + '</span>';
-		icon    = 'icon-picture';
-		break;
-
-	case "quote":
-		content = '<blockquote>' + post['quote-text'] + '</blockquote>';
-		icon    = 'icon-quote-right';
-		break;
-
-	case "audio":
-		content = post['audio-player'] + '<span class="song-caption">' + post['audio-caption'] + '</span>';
-		icon    = 'icon-music';
-		break;
-	}
-
-	// Send it to the view
-	$('#main').append('<div class="post"><span class="post-date">' + date + '</span><i class="' + icon + '"></i><div class="content">' + content + '</div></div>');
-
-
-	// Remove duplicate dates
-	// http://stackoverflow.com/a/2822974
-	var seen = {};
-	$('.post-date').each(function() {
-		var txt = $(this).text();
-		if (seen[txt])
-			$(this).hide().next('content').addClass('bordered');
-		else
-			seen[txt] = true;
+	$.getJSON(tumblr_feed, function(data) {
+		getPosts(data.posts);
 	});
+
+	// HELPERS
+	function getPosts(json) {
+		for( var i=0, l=json.length; i<l; i++ ) {
+			formatPost(json[i]);
+		}
+	}
+
+	function formatPost(post) {
+		date    = moment.unix(post['unix-timestamp']).format('MMMM Do YYYY');
+		content = post['regular-body'];
+		type    = post.type;
+
+		// Format post based on post type
+		switch (type)
+		{
+		// Normal post
+		case "regular":
+			content = '<h3>' + post['regular-title'] + '</h3>' + post['regular-body'];
+			icon    = 'icon-align-left';
+			break;
+
+		// Link post
+		case "link":
+			content = '<span class="link-text"><a href="' + post['link-url'] + 
+			          '" target="_blank">' + post['link-text'] + '</a></span>' +
+			          '<span class="link-description"> ' + post['link-description'] + '</span>';
+			icon    = 'icon-link';
+			break;
+
+		// Video post
+		case "video":
+			content = '<div class="video-container">' + post['video-player-500'] + '</div><span class="video-caption">' + post['video-caption'] + '</span>';
+			icon    = 'icon-film';
+			break;
+
+		// Photo post
+		case "photo":
+			content = '<div class="photo"><img src="' + post['photo-url-1280'] + 
+			          '" alt=""></div>' +
+			          '<span class="photo-caption">' + post['photo-caption'] + '</span>';
+			icon    = 'icon-picture';
+			break;
+
+		case "quote":
+			content = '<blockquote>' + post['quote-text'] + '</blockquote>';
+			icon    = 'icon-quote-right';
+			break;
+
+		case "audio":
+			content = post['audio-player'] + '<span class="song-caption">' + post['audio-caption'] + '</span>';
+			icon    = 'icon-music';
+			break;
+		}
+
+		// Send it to the view
+		$('#main').append('<div class="post"><span class="post-date">' + date + '</span><i class="' + icon + '"></i><div class="content">' + content + '</div></div>');
+
+
+		// Remove duplicate dates
+		// http://stackoverflow.com/a/2822974
+		var seen = {};
+		$('.post-date').each(function() {
+			var txt = $(this).text();
+			if (seen[txt])
+				$(this).hide().next('content').addClass('bordered');
+			else
+				seen[txt] = true;
+		});
+	}
 }
